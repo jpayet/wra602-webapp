d745489b96e78d8f2cd578eae78f15352533c15b
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2024-06-12 23:05:29 +0200

    feat: new Page History

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\PdfRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HistoryController extends AbstractController
 {
+    private PdfRepository $pdfRepository;
+
+    public function __construct(PdfRepository $pdfRepository)
+    {
+        $this->pdfRepository = $pdfRepository;
+    }
+
     #[Route('/history', name: 'app_history')]
     public function index(): Response
     {
+        $pdfs = $this->pdfRepository->findBy(['user' => $this->getUser()]);
+
         return $this->render('index/history.html.twig', [
-            'controller_name' => 'HistoryController',
+            'pdfs' => $pdfs
         ]);
     }
 }
