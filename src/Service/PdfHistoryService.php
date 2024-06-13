01981d12d1cfab92b416cb16d0f8fcef4601d1cf
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2024-06-13 23:44:02 +0200

    feat: send mail with pdf attachment on generation sucess

--- src/Service/PdfHistoryService.php
+++ src/Service/PdfHistoryService.php
@@ -4,16 +4,21 @@ namespace App\Service;
 
 use App\Entity\Pdf;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use \Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Mailer\MailerInterface;
+
 class PdfHistoryService
 {
     private $entityManager;
     private $security;
+    private $mailer;
 
-    public function __construct(EntityManagerInterface $entityManager, Security $security)
+    public function __construct(EntityManagerInterface $entityManager, Security $security, MailerInterface $mailer)
     {
         $this->entityManager = $entityManager;
         $this->security = $security;
+        $this->mailer = $mailer;
     }
 
     public function savePdf($filename, $method)
@@ -26,5 +31,21 @@ class PdfHistoryService
 
         $this->entityManager->persist($pdf);
         $this->entityManager->flush();
+    }
+
+    public function sendPdf($pdfContent, $filename, $user) {
+        $username = $user->getFirstName() . ' ' . $user->getLastName();
+        $email = (new TemplatedEmail())
+            ->from('no-reply@pdfraptor.com')
+            ->to($user->getEmail())
+            ->subject('Votre PDF est prêt !')
+            ->text('Bonjour, votre PDF est prêt !')
+            ->htmlTemplate('generate_pdf/generated_pdf_email.html.twig')
+            ->context([
+                'username' => $username,
+            ])
+            ->attach($pdfContent, $filename, 'application/pdf');
+
+        $this->mailer->send($email);
     }
 }
